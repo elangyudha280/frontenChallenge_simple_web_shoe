aa138f49ae0d3ba9746e89a1d53ccbbc62b66135
=elangyudha280 <=elangyudharakasiwi2@gmail.com>
2024-07-25 19:44:03 -0700

    selesai fitur slider

--- components/section/section.jsx
+++ components/section/section.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 
 // imoprt pkg
 import Slider from 'react-slick'
-
+import { FaArrowRight,FaArrowLeft } from "react-icons/fa6";
 // import img
 import iconShoes from '@/assets/images/shoes_1.svg'
 
@@ -15,7 +15,7 @@ import useShoes from '@/utils/dataShoes'
 
 export function SectionCardListHome() {
   let daftarSepatu = useShoes(state => state.daftarSepatu)
-
+  const [currentSlide, setCurrentSlide] = useState(0);
     let sliderRef = useRef(null);
   const next = () => {
     sliderRef.slickNext();
@@ -27,9 +27,10 @@ export function SectionCardListHome() {
     dots: false,
     infinite: false,
     draggable:true,
-    speed: 700,
+    speed: 200,
     slidesToShow: 4,
     slidesToScroll: 2,
+    afterChange: (current) => setCurrentSlide(current),
     responsive:[
       {
         breakpoint: 1151,
@@ -56,8 +57,32 @@ export function SectionCardListHome() {
       },
     ]
   };
+  useEffect(() => {
+    console.log(currentSlide)
+  }, [currentSlide])
+  
   return (
     <div className="relative w-full mt-3">
+      {/* button nav slide */}
+      <div className="relative w-full my-4  flex gap-3 justify-end min-[1151px]:hidden">
+            {/* btn previous */}
+            <button onClick={previous} type='button' className={`
+            flex gap-2 flex-wrap justify-center items-center p-2 font-semibold  rounded-md transition-all duration-200 max-[320px]:w-full 
+            ${currentSlide === 0 ? ' text-slate-950  bg-transparent ring-2 ring-custom-black-800' : 'text-white bg-custom-black-800 ring-0  hover:text-slate-950  hover:bg-transparent hover:ring-2 hover:ring-custom-black-800'}
+              `}>
+                  <FaArrowLeft className='text-[12px] mt-0.5'/>
+            </button>
+
+            {/* btn next */}
+            <button onClick={next} type='button' className={`
+              flex gap-2 flex-wrap justify-center items-center p-2 font-semibold  rounded-md transition-all duration-200 max-[320px]:w-full
+               ${currentSlide ===1 ? ' text-slate-950  bg-transparent ring-2 ring-custom-black-800' : 'text-white bg-custom-black-800 ring-0  hover:text-slate-950  hover:bg-transparent hover:ring-2 hover:ring-custom-black-800'}
+              `}>
+                  <FaArrowRight className='text-[12px] mt-0.5'/>
+            </button>
+      </div>
+
+      {/* slider */}
         <div className="slider-container">
         <Slider
             ref={slider => {
@@ -69,7 +94,7 @@ export function SectionCardListHome() {
             daftarSepatu?.map((el,i)=>{
               return (
                 <React.Fragment key={el.id}>
-                    <div className="relative w-[98%] min-[665px]:w-[95%] min-[860px]:w-[270px] ">
+                    <div className="relative w-[98%] min-[665px]:w-[95%] min-[1151px]:w-[270px] ">
                       {/* card header list shoes */}
                       <div className="card_header_list_shoes relative w-full h-auto min-[665px]:h-[300px] overflow-hidden rounded-2xl">
                           <Image alt='' src={el.poster} className='w-full h-full object-cover object-center' />
